9f4f0d8ab29c1f343e177d7efc4b6cca99e8797a
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2015-11-09 07:26:40 +0100

    error handling refactoring

--- src/main/java/pl/p/lodz/iis/hr/controllers/AuthController.java
+++ src/main/java/pl/p/lodz/iis/hr/controllers/AuthController.java
@@ -49,12 +49,7 @@ class AuthController {
     @RequestMapping(
             value = "/login-issue",
             method = RequestMethod.GET)
-    public String loginIssue(HttpServletRequest request,
-                             HttpServletResponse response) {
-
-        Pac4jSecurityHelper pac4jSecurityHelper = new Pac4jSecurityHelper(gitHubClient, request, response);
-        pac4jSecurityHelper.getProfileManager().remove(true);
-
+    public String loginIssue() {
         return "main-login-issue";
     }
 }
