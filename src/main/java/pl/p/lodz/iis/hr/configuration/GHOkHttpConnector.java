de91770b9f34ad8a2844b53cb2108ad5cbdac4c9
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2015-11-19 01:34:21 +0100

    fix cache controlling. cache should works now

--- src/main/java/pl/p/lodz/iis/hr/configuration/GHOkHttpConnector.java
+++ src/main/java/pl/p/lodz/iis/hr/configuration/GHOkHttpConnector.java
@@ -12,12 +12,12 @@ import java.net.URL;
  * - Set User-Agent header as requested by GitHub.<br/>
  * - Explicitly set Accept header as encouraged by GitHub.<br/>
  *
- * - Set Cache-control as "max-age=0, must-revalidate" to bypass header sent by gihub api,
- *   which tells "private, max-age=60, s-maxage=60" - so client should not sent request,
- *   even if with "if-none-match" header, just use cache.
+ * - Set own Cache-control to bypass header sent by gihub api,
+ *   which tells "private, max-age=60, s-maxage=60",
+ *   so client should not/cannot(?) sent request within 60s, even with "if-none-match" header, just use cache.
  *
- *   OkHttpClient now will use my cache-control directive - will sent request every time,
- *   but still will ask if resource is modified, and use cache if not.
+ *   OkHttpClient now will use my cache-control directive "max-age=0, must-revalidate"
+ *   - will sent request every time, but still will ask if resource is modified, and use cache if not.
  */
 class GHOkHttpConnector extends OkHttpConnector {
 
