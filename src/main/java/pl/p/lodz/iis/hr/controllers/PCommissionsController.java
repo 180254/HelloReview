697bc13be33b85973b9dbf1bc9d06c385b71cfa7
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2015-11-22 19:02:09 +0100

    p-commissions: current draft of displaying review form

--- src/main/java/pl/p/lodz/iis/hr/controllers/PCommissionsController.java
+++ src/main/java/pl/p/lodz/iis/hr/controllers/PCommissionsController.java
@@ -3,9 +3,13 @@ package pl.p.lodz.iis.hr.controllers;
 import org.pac4j.oauth.client.GitHubClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import pl.p.lodz.iis.hr.configuration.GHPac4jSecurityHelper;
+import pl.p.lodz.iis.hr.exceptions.ErrorPageException;
 import pl.p.lodz.iis.hr.models.courses.Commission;
 import pl.p.lodz.iis.hr.models.courses.Participant;
 import pl.p.lodz.iis.hr.repositories.CommissionRepository;
@@ -14,6 +18,7 @@ import pl.p.lodz.iis.hr.repositories.ParticipantRepository;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class PCommissionsController {
@@ -23,17 +28,55 @@ public class PCommissionsController {
     @Autowired private CommissionRepository commissionRepository;
 
     @RequestMapping("/p/commissions")
+    @Transactional
     public String commissions(HttpServletRequest request,
                               HttpServletResponse response,
                               Model model) {
 
-        GHPac4jSecurityHelper ghSecurityHelper = new GHPac4jSecurityHelper(gitHubClient, request, response);
-        String username = ghSecurityHelper.getUserProfileFromSession().getUsername();
-
-        List<Participant> byGitHubName = participantRepository.findByGitHubName(username);
+        List<Participant> byGitHubName = meAsParticipant(request, response);
         List<Commission> byAssessorIn = commissionRepository.findByAssessorIn(byGitHubName);
 
         model.addAttribute("commissions", byAssessorIn);
         return "p-commissions";
+    }
+
+
+    @RequestMapping(
+            value = "/p/commissions/{uuid}",
+            method = RequestMethod.GET)
+    @Transactional
+    public String form(@PathVariable String uuid,
+                       HttpServletRequest request,
+                       HttpServletResponse response,
+                       Model model)
+            throws ErrorPageException {
+
+        UUID uuid1 = getUuidFromString(uuid);
+
+        Commission byUuid = commissionRepository.findByUuid(uuid1);
+        if (byUuid == null) {
+            throw new ErrorPageException(HttpServletResponse.SC_NOT_FOUND);
+        }
+
+        List<Participant> byGitHubName = meAsParticipant(request, response);
+        if (!byGitHubName.contains(byUuid.getAssessor())) {
+            throw new ErrorPageException(HttpServletResponse.SC_FORBIDDEN);
+        }
+
+        return "p-form";
+    }
+
+    private UUID getUuidFromString(String uuid) throws ErrorPageException {
+        try {
+            return UUID.fromString(uuid);
+        } catch (IllegalArgumentException e) {
+            throw (ErrorPageException) new ErrorPageException(HttpServletResponse.SC_NOT_FOUND).initCause(e);
+        }
+    }
+
+    private List<Participant> meAsParticipant(HttpServletRequest request, HttpServletResponse response) {
+        GHPac4jSecurityHelper ghSecurityHelper = new GHPac4jSecurityHelper(gitHubClient, request, response);
+        String username = ghSecurityHelper.getUserProfileFromSession().getUsername();
+        return participantRepository.findByGitHubName(username);
     }
 }
