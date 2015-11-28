d65a06c6474595bd863ef7b7cfaa810da92e1a4b
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2015-11-29 00:59:41 +0100

    implemented csrf token provider

--- src/main/java/pl/p/lodz/iis/hr/controllers/IndexController.java
+++ src/main/java/pl/p/lodz/iis/hr/controllers/IndexController.java
@@ -2,9 +2,16 @@ package pl.p.lodz.iis.hr.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.ServletRequest;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Nothing special, index must be displayable.<br/>
@@ -27,5 +34,25 @@ class IndexController {
             method = RequestMethod.GET)
     public String gitHubIssue() {
         return "github-issue";
+    }
+
+    @RequestMapping(
+            value = "/csrf-token",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public List<String> csrfToken(ServletRequest request) {
+        Object csrf = request.getAttribute("_csrf");
+
+        if (!(csrf instanceof CsrfToken)) {
+            return Arrays.asList(null, null, null);
+        }
+
+        CsrfToken csrfToken = (CsrfToken) csrf;
+        return Arrays.asList(
+                csrfToken.getToken(),
+                csrfToken.getHeaderName(),
+                csrfToken.getParameterName()
+        );
     }
 }
